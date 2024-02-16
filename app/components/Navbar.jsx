fa25dc1279c43760a104150cc625553c8a9c70ed
Nicholas Correa <nickcorrea33@gmail.com>
2024-02-16 09:34:16 -0800

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -11,8 +11,8 @@ const navLinks = [
 const Navbar = () => {
     return (
         <nav>
-            <div className="flex flex-wrap items-center justify-between mx-auto px-8">
-                <Link href="/" className="text-5xl text-white font-semibold">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+                <Link href="/" className="text-5xl text-black font-semibold">
                     LOGO
                 </Link>
                 <div className="menu hidden md:block md:w-auto" id="navbar">
